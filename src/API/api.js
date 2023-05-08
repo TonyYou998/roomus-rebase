672cd748ea9f62a9fa1b991cc08f6cf69d4a83e2
Andy Koi <anhkhoi1192002@gmail.com>
2023-05-08 21:28:25 +0700

    change url be deployed

--- src/API/api.js
+++ src/API/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const mainApi=axios.create({
    
-    // baseURL:"https://roomusapi.gzfjbzbcamc8aehr.southeastasia.azurecontainer.io:3001/api/v1",
-    baseURL:"http://localhost:3001/api/v1",
+    baseURL:"https://roomusapi.gzfjbzbcamc8aehr.southeastasia.azurecontainer.io:3001/api/v1",
+    // baseURL:"http://localhost:3001/api/v1",
 });
 
 export {mainApi}
